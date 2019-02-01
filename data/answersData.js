a80b55a8de5ed16601dd0fee18b6f8a48f9bfced
Rivki Aizen <rivkiaizen@gmail.com>
2019-02-01 10:26:16 +0200

    set AnswersData constructor

--- data/answersData.js
+++ data/answersData.js
@@ -1,11 +1,11 @@
 class AnswersData {
-    constructor(location, crop, variety, date) {
-        this.picture = picture || undefined;
+    constructor(crop, variety, plantingDate, location) {
+        this.picture = undefined;
         this.crop = crop || undefined;
         this.variety = variety || undefined;
         this.plantingDate = plantingDate || undefined;
         this.location = location || undefined;
-        this.symptomLocation = symptomLocation || [];
+        this.symptomLocation =  [];
     }
 };
 
