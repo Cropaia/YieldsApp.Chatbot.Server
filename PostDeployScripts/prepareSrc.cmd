a75eb0d54054793839056aea1dbd44ae2d1bef44
Rivki Aizen <rivki403@gmail.com>
2019-04-28 20:17:59 +0300

    connect to new project

--- PostDeployScripts/prepareSrc.cmd
+++ PostDeployScripts/prepareSrc.cmd
@@ -1,6 +1,7 @@
-rem @echo off
+@echo off
 setlocal
 SET password=%1
+SET exportSettings=%2
 SET repoName=srcRepo
 SET repoUrl=file:///%HOMEDRIVE:~0,1%/%HOMEPATH:~1%/site/%repoName%
 SET download=bot-src
@@ -24,7 +25,7 @@ pushd %HOME%\site
 mkdir srcRepo
 cd srcRepo
 call git init --bare
-popd 
+popd
 
 rem push to upstream
 pushd ..\wwwroot
@@ -40,8 +41,30 @@ call rm -r -f .git
 popd
 
 rem prepare for publish
-type PostDeployScripts\publish.js.template | sed -e s/\{WEB_SITE_NAME\}/%WEBSITE_SITE_NAME%/g | sed -e s/\{PASSWORD\}/%password%/g > %HOME%\site\%download%\publish.js
+pushd %HOME%\site\%download%
+mkdir Properties\PublishProfiles
+pushd Properties\PublishProfiles
+type ..\..\PostDeployScripts\publishProfile.xml.template | sed -e s/\{WEB_SITE_NAME\}/%WEBSITE_SITE_NAME%/g > %WEBSITE_SITE_NAME%-Web-Deploy.pubxml
+popd
 
+set SOLUTION_NAME=
+for /f "delims=" %%a in ('dir /b *.sln') do @set SOLUTION_NAME=%%a
+
+type PostDeployScripts\publish.cmd.template | sed -e s/\{SOLUTION_NAME\}/%SOLUTION_NAME%/g | sed -e s/\{PUBLISH_PROFILE\}/%WEBSITE_SITE_NAME%-Web-Deploy.pubxml/g | sed -e s/\{PASSWORD\}/%password%/g > publish.cmd
+type PostDeployScripts\publishSettings.xml.template | sed -e s/\{WEB_SITE_NAME\}/%WEBSITE_SITE_NAME%/g | sed -e s/\{PASSWORD\}/%password%/g > PostDeployScripts\%WEBSITE_SITE_NAME%.PublishSettings
+
+popd
+
+echo %exportSettings%
+
+if "%exportSettings%" == "" goto zip
+pushd %HOME%\site\%download%
+echo 'export app settings'
+call :APP_SETTINGS
+popd
+
+
+:zip
 rem preare the zip file
 %HOMEDRIVE%\7zip\7za a %HOME%\site\%download%.zip %HOME%\site\%download%\*
 
@@ -56,3 +79,10 @@ call rm -r -f %repoName%
 popd
 
 endlocal
+
+goto :EOF
+
+:APP_SETTINGS
+echo 'dump app settings'
+node %HOME%\site\wwwroot\PostDeployScripts\mergeSettings.js
+exit /b
